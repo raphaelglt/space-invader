5425b412801a981ca61f84c18053b8e46be8882a
raphaelglt <raphael.guillot@edu.esiee-it.fr>
2023-02-06 15:09:01 +0100

    moving player

--- app.js
+++ app.js
@@ -5,62 +5,54 @@ let Player = class Player {
   }
 }
 
-const grid = document.getElementsByClassName('grille')[0]
-for (let i = 0; i<12; i++) {
-  const startDiv = document.createElement('div');
-  startDiv.setAttribute('start', true);
-  grid.appendChild(startDiv)
-  for (let j = 0; j<18; j++) {
-    const newDiv = document.createElement('div');
-    grid.appendChild(newDiv)
-  }
-  const endDiv = document.createElement('div');
-  endDiv.setAttribute('end', true);
-  grid.appendChild(endDiv)
-}
-
 const squares = grid.children;
+var gridTop = grid.getBoundingClientRect().top;
+var gridBottom = grid.getBoundingClientRect().bottom;
+var gridLeft = grid.getBoundingClientRect().left;
+var gridRight = grid.getBoundingClientRect().right;
 
 function generatePlayer() {
-  const playerImg = document.createElement('img')
-  playerImg.setAttribute('src', './ressources/vaisseau.png')
-  playerImg.setAttribute('height', '100%')
-  playerImg.setAttribute('width', '100%')
-  playerImg.setAttribute('style', 'transform: rotate(180deg)')
-  squares[229].appendChild(playerImg)
-  return new Player(229, playerImg)
+  const playerImg = document.createElement('div')
+  grid.appendChild(playerImg)
+  playerImg.setAttribute('class', 'tireur')
+  return new Player({bottom: gridBottom-50, left: (gridRight/2)+50}, playerImg)
 }
 const player = generatePlayer()
+player.elt.style.top = player.pos.bottom+"px";
+player.elt.style.left = player.pos.left+"px";
 
-console.log(squares.length)
+console.log(gridTop, gridBottom, gridLeft, gridRight)
+const speed = 15;
 
 document.addEventListener("keydown", (event) => {
   if (event.isComposing || event.keyCode === 37) {
-    if (player.elt.parentNode.getAttribute('start') != "true") {
-      squares[player.pos].removeChild(player.elt);
-      squares[player.pos-1].appendChild(player.elt)
-      player.pos -= 1
+    if (player.pos.left > gridLeft+5) {
+      grid.removeChild(player.elt);
+      player.pos.left -= speed;
+      player.elt.style.left = player.pos.left+"px"
+      grid.appendChild(player.elt)
     }  
   }
   if (event.isComposing || event.keyCode === 39) {
-    if (player.elt.parentNode.getAttribute('end') != "true") {
-      squares[player.pos].removeChild(player.elt);
-      squares[player.pos+1].appendChild(player.elt)
-      player.pos += 1
+    if (player.pos.left < gridRight-50) {
+      grid.removeChild(player.elt);
+      player.pos.left += speed;
+      player.elt.style.left = player.pos.left+"px"
+      grid.appendChild(player.elt)
     }  
   }
   if (event.isComposing || event.keyCode === 40) {
     if (player.pos < 220) {
       squares[player.pos].removeChild(player.elt);
       squares[player.pos+20].appendChild(player.elt)
-      player.pos += 20
+      player.pos += speed
     }  
   }
   if (event.isComposing || event.keyCode === 38) {
-    if (player.pos > 19) {
+    if (player.pos > 1) {
       squares[player.pos].removeChild(player.elt);
       squares[player.pos-20].appendChild(player.elt)
-      player.pos -= 20
+      player.pos -= speed
     }  
   }
 });
