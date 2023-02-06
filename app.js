db28292ab8c731ff361a35e025e9b6536d41451e
adamboissy2004@gmail.com <adamboissy2004@gmail.com>
2023-02-06 14:54:51 +0100

    test 4 deplacement ennemie

--- app.js
+++ app.js
@@ -10,3 +10,29 @@ for (let i = 0; i<12; i++) {
   endDiv.setAttribute('end', true);
   grid.appendChild(endDiv)
 }
+
+
+    var vaisseau = document.getElementById("ennemi");
+    var vaisseauLeft = vaisseau.offsetLeft;
+    var vaisseauTop = vaisseau.offsetTop;
+    var direction = 1;
+
+    // Boucle de mouvement
+    setInterval(function() {
+        const style = getComputedStyle(grid);
+      vaisseauLeft += 10 * direction;
+      console.log("console : "+style.width)
+      if (vaisseauLeft >= window.innerWidth - 20 || vaisseauLeft <= 0) {
+        direction *= -1;
+        vaisseauTop += 20;
+      }
+      vaisseau.style.left = vaisseauLeft + "px";
+      vaisseau.style.top = vaisseauTop + "px";
+    }, 100);
+
+
+    
+    
+
+
+
