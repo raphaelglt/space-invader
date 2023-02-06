4ea6312596f3d28708c8e32a9d95318a59c0fc82
nom1 <nom1@gmail.com>
2023-02-06 16:11:48 +0100

    ajout home page

--- defeteJoueur.js
+++ defeteJoueur.js
@@ -1,6 +1,8 @@
 var casejoueur;
 var ennemis;
 var numberOfennemis;
+var scoreUser;
+var namePlayer;
 function defetejoueur(){
     if (casejoueur==ennemis) {
         return "Vous avez perdu";
@@ -8,6 +10,11 @@ function defetejoueur(){
 }
 function winPlayer(){
     if(numberOfennemis==0){
-        return "aucun ennemis sur la carte. vous avez gagné!"
+        return "aucun ennemis sur la carte. vous avez gagné! voici votre score:"
     }
 }
+
+localStorage.setItem("score", scoreUser);
+localStorage.getItem("score");
+localStorage.setItem("namePlayer", namePlayer);
+localStorage.getItem("namePlayer");
