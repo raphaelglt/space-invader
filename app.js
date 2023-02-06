5a2c8502b8ee0397e403391e9ed16b4ac3a3c128
raphaelglt <raphael.guillot@edu.esiee-it.fr>
2023-02-06 13:24:33 +0100

    player move

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+let Player = class Player {
+  constructor(pos, elt) {
+    this.elt = elt
+    this.pos = pos;
+  }
+}
+
 const grid = document.getElementsByClassName('grille')[0]
 for (let i = 0; i<12; i++) {
   const startDiv = document.createElement('div');
+  startDiv.setAttribute('start', true);
   grid.appendChild(startDiv)
   for (let j = 0; j<18; j++) {
     const newDiv = document.createElement('div');
@@ -20,6 +28,39 @@ function generatePlayer() {
   playerImg.setAttribute('width', '100%')
   playerImg.setAttribute('style', 'transform: rotate(180deg)')
   squares[229].appendChild(playerImg)
-  console.log(squares[200])
+  return new Player(229, playerImg)
 }
-generatePlayer() 
+const player = generatePlayer()
+
+console.log(squares.length)
+
+document.addEventListener("keydown", (event) => {
+  if (event.isComposing || event.keyCode === 37) {
+    if (player.elt.parentNode.getAttribute('start') != "true") {
+      squares[player.pos].removeChild(player.elt);
+      squares[player.pos-1].appendChild(player.elt)
+      player.pos -= 1
+    }  
+  }
+  if (event.isComposing || event.keyCode === 39) {
+    if (player.elt.parentNode.getAttribute('end') != "true") {
+      squares[player.pos].removeChild(player.elt);
+      squares[player.pos+1].appendChild(player.elt)
+      player.pos += 1
+    }  
+  }
+  if (event.isComposing || event.keyCode === 40) {
+    if (player.pos < 220) {
+      squares[player.pos].removeChild(player.elt);
+      squares[player.pos+20].appendChild(player.elt)
+      player.pos += 20
+    }  
+  }
+  if (event.isComposing || event.keyCode === 38) {
+    if (player.pos > 19) {
+      squares[player.pos].removeChild(player.elt);
+      squares[player.pos-20].appendChild(player.elt)
+      player.pos -= 20
+    }  
+  }
+});
