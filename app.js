7202029f21fef2d60be1c34684632d33f3c9e77d
raphaelglt <raphael.guillot@edu.esiee-it.fr>
2023-02-06 11:43:41 +0100

    player img

--- app.js
+++ app.js
@@ -10,3 +10,16 @@ for (let i = 0; i<12; i++) {
   endDiv.setAttribute('end', true);
   grid.appendChild(endDiv)
 }
+
+const squares = grid.children;
+
+function generatePlayer() {
+  const playerImg = document.createElement('img')
+  playerImg.setAttribute('src', './ressources/vaisseau.png')
+  playerImg.setAttribute('height', '100%')
+  playerImg.setAttribute('width', '100%')
+  playerImg.setAttribute('style', 'transform: rotate(180deg)')
+  squares[229].appendChild(playerImg)
+  console.log(squares[200])
+}
+generatePlayer() 
