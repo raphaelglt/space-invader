b2b40113f41ecdbc079e27791919b2c715ce051f
nom1 <nom1@gmail.com>
2023-02-06 14:13:57 +0100

    playerWinner ajouter

--- defeteJoueur.js
+++ defeteJoueur.js
@@ -1,7 +1,13 @@
 var casejoueur;
 var ennemis;
+var numberOfennemis;
 function defetejoueur(){
     if (casejoueur==ennemis) {
         return "Vous avez perdu";
+    }
+}
+function winPlayer(){
+    if(numberOfennemis==0){
+        return "aucun ennemis sur la carte. vous avez gagné!"
     }
 }
