ff1c7c5e7d83b28620dd795a762d87008906f367
adamboissy2004@gmail.com <adamboissy2004@gmail.com>
2023-02-07 12:52:59 +0100

    rebase

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ for (let i = 0; i<12; i++) {
   const endDiv = document.createElement('div');
   endDiv.setAttribute('end', true);
   grid.appendChild(endDiv)
+}const grid = document.getElementsByClassName('grille')[0]
+for (let i = 0; i<12; i++) {
+  const startDiv = document.createElement('div');
+  grid.appendChild(startDiv)
+  for (let j = 0; j<18; j++) {
+    const newDiv = document.createElement('div');
+    grid.appendChild(newDiv)
+  }
+  const endDiv = document.createElement('div');
+  endDiv.setAttribute('end', true);
+  grid.appendChild(endDiv)
 }
 
 
