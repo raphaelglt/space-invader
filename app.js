f6c96ab938f38810efb691d22a3c9b65270ec8e4
adamboissy2004@gmail.com <adamboissy2004@gmail.com>
2023-02-07 12:58:43 +0100

    rep

--- app.js
+++ app.js
@@ -9,17 +9,6 @@ for (let i = 0; i<12; i++) {
   const endDiv = document.createElement('div');
   endDiv.setAttribute('end', true);
   grid.appendChild(endDiv)
-}const grid = document.getElementsByClassName('grille')[0]
-for (let i = 0; i<12; i++) {
-  const startDiv = document.createElement('div');
-  grid.appendChild(startDiv)
-  for (let j = 0; j<18; j++) {
-    const newDiv = document.createElement('div');
-    grid.appendChild(newDiv)
-  }
-  const endDiv = document.createElement('div');
-  endDiv.setAttribute('end', true);
-  grid.appendChild(endDiv)
 }
 
 
@@ -40,6 +29,9 @@ for (let i = 0; i<12; i++) {
       vaisseau.style.left = vaisseauLeft + "px";
       vaisseau.style.top = vaisseauTop + "px";
     }, 100);
+
+
+   
 
 
    
