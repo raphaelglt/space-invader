c836a9273dd630dda4756dea07826fcc0864ed0f
adamboissy2004@gmail.com <adamboissy2004@gmail.com>
2023-02-08 09:19:09 +0100

    pull

--- entity.js
+++ entity.js
@@ -33,13 +33,7 @@ export let Entity = class Entity {
     grid.appendChild(this.elt)
 
     
-  player.forEach(enemy => {
-    let distance = Math.sqrt(Math.pow(this.pos.left - enemy.pos.left, 2) + Math.pow(this.pos.bottom - enemy.pos.bottom, 2));
-    if (distance < 30) {
-      
-      alert("Vous avez été touché par un ennemi ! Vous avez perdu.");
-    }
-  });
+  
 
 
     
